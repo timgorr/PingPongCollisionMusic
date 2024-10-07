7ab917d52bf082231ee8a1d628ac0503d81346cc
Tim Gorr <TGorr01@gmail.com>
2024-10-07 08:48:52 +0200

    day size ball change

--- try.js
+++ try.js
@@ -3,14 +3,18 @@ let video;
 let hands = [];
 let pentatonicScale = ["C4", "D4", "E4", "G4", "A4"]; 
 let synth = new Tone.Synth().toDestination(); 
+let sadness;
+let happiness;
+let anxiety;
 
 let collisionIndex = 10;
-
 
 let x = 100;
 let y = 100;
 let speedX = 5;
 let speedY = 8;
+
+let ellipseRadius = 80;
 
 function preload() {
   handpose = ml5.handPose({ flipHorizontal: true });
@@ -22,23 +26,23 @@ function setup() {
   video.size(640, 480);
   video.hide();
   
-
   handpose.detectStart(video, getHandsData);
 }
-
-
 
 let lastCollisionTime = 0; 
 const collisionCooldown = 300; 
 
 function draw() {
+  const d = new Date();
+  let day = weekday[d.getDay()];
+  checkWeekday(day);
+
   push();
   scale(-1,1);
   translate(-width, 0);
   image(video, 0, 0, width, height);
   pop();
   drawGrid(8, 6);
-
 
   for (let hand of hands) {
     let indexFinger = hand.index_finger_tip;
@@ -49,7 +53,6 @@ function draw() {
 
     let distance = dist(indexFinger.x, indexFinger.y, thumb.x, thumb.y);
 
-    // finger ellipse code from lecture
     if (distance < 50) { 
       noStroke();
       fill(0, 0, 255, 150);
@@ -61,7 +64,6 @@ function draw() {
       playNoteFromGrid(gridX, gridY);
     }
 
-    // Hand collision
     if (x > indexFinger.x && x < indexFinger.x + 50 && y > indexFinger.y && y < indexFinger.y + 50) {
       let currentTime = millis();
       if (currentTime - lastCollisionTime > collisionCooldown) {
@@ -72,17 +74,11 @@ function draw() {
         collisionIndex = collisionIndex * 3;
       }
     }
-    // if(collisionIndex >= 255) {
-    //   playNoteFromGrid(x++, y++);
-    // }
   }
 
-  ellipseRadius = 80;
   fill(collisionIndex, 0, 0);
   ellipse(x, y, ellipseRadius);
 
-
-  // Border collision
   let currentTime = millis();
   if (x + (ellipseRadius / 2) > width || x < 0 + (ellipseRadius / 2)) {
     if (currentTime - lastCollisionTime > collisionCooldown) {
@@ -103,7 +99,6 @@ function draw() {
   y += speedY;
 }
 
-
 function drawGrid(cols, rows) {
   stroke(255, 150); 
   for (let i = 0; i <= cols; i++) {
@@ -123,4 +118,34 @@ function playNoteFromGrid(x, y) {
 
 function getHandsData(results) {
   hands = results;
+}
+
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+function checkWeekday(day) {
+  switch(day) {
+    case "Monday":
+      ellipseRadius = 50;
+      break;
+    case "Tuesday":
+      ellipseRadius = 70;
+      break;
+    case "Wednesday":
+      ellipseRadius = 80;
+      break;
+    case "Thursday":‚
+      ellipseRadius = 100;
+      break;
+    case "Friday":
+      ellipseRadius = 120;
+      break;
+    case "Saturday":
+      ellipseRadius = 150;
+      break;
+    case "Sunday":
+      ellipseRadius = 180;
+      break;
+    default:
+      ellipseRadius = 80;
+  }
 }
